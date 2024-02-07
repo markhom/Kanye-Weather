d81532f7f1ef91df34ae03bf39d82a6524c29463
Amara <ajbriquelet@gmail.com>
2024-02-06 19:08:34 -0600

    eventlistener and weather function skeleten

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+//link to button, listen for click, call anon function to start chain of functions that should run sequentially but appear as if loading simultaneously to the user. 
+document.getElementById("clickEvent").addEventListener("click", function(){ 
+    quoteGenerator()
+});
+function quoteGenerator() {
 const url = 'https://api.kanye.rest/'; //url variable name assigned a value of: https://api.kanye.rest/
 
 fetch(url) //request quotes from server
@@ -5,4 +10,11 @@ fetch(url) //request quotes from server
 .then(data => {
     document.getElementById('quoteContainer').textContent = data.quote; // id for injected quotes is "quoteContainer". 
 })
-.catch(error => console.error(error)); //error message.     
+.catch(error => console.error(error)); //error message.    
+//adding a call to the weather forecast function here will allow it to execute sequentially.
+//EXAMPLE:
+// getWeather()
+//must implement a way to locate the users city or chosen city
+}
+
+//INSERT 5 DAY WEATHER FORECAST/GEOCODING(???MAYBE NEED?)
