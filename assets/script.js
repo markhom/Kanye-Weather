bcca5c3585123cb8f3a3a2161b1b9ecdc89045e8
Amara <ajbriquelet@gmail.com>
2024-02-06 21:00:42 -0600

    asks user for permission for location

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,38 @@ document.getElementById("clickEvent").addEventListener("click", function(){
 });
 function quoteGenerator() {
 const url = 'https://api.kanye.rest/'; //url variable name assigned a value of: https://api.kanye.rest/
-
 fetch(url) //request quotes from server
 .then(response => response.json()) //json conversion 
 .then(data => {
     document.getElementById('quoteContainer').textContent = data.quote; // id for injected quotes is "quoteContainer". 
+    //call function to automatically detect user location
+    getLocation();
 })
 .catch(error => console.error(error)); //error message.     
 }
+//function to retrieve user coordinates
+ function getLocation() {
+    let userLocate = navigator.geolocation.watchPosition(
+        successCall, //success condition
+        errorCall, //error only here so I can include the high accuracy object. 
+        {
+            enableHighAccuracy:true, //options object, higher accuracy geolocation.
+        }  
+        );
+        // return userLocate;
+ }
 
+ function successCall(position) {
+let latitude = position.coords.latitude;
+let longitude = position.coords.longitude;
+
+//Nearest city based on coords
+fetch
+ }
+
+ function errorCall(error) {
+
+ }
 
 $(".modal-button").click(function() {
     var target = $(this).data("target");
