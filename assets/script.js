296e049ec86443077f59d5fbc1fac1c346905fef
feli619 <152445873+feli619@users.noreply.github.com>
2024-02-08 20:49:55 -0500

    edits to html and js for modal

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,15 @@
+//scrpit for modal
+const modal = $(".modal-button").click(function() {
+    var target = $(this).data("target");
+    $("html").addClass("is-clipped");
+    $(target).addClass("is-active");
+ });
+ 
+ $(".modal-close").click(function() {
+    $("html").removeClass("is-clipped");
+    $(this).parent().removeClass("is-active");
+ });
+
 //link to button, listen for click, call anon function to start chain of functions that should run sequentially but appear as if loading simultaneously to the user. 
 document.getElementById("clickEvent").addEventListener("click", function(){ 
     quoteGenerator()
@@ -12,19 +24,6 @@ fetch(url) //request quotes from server
 })
 .catch(error => console.error(error)); //error message.     
 }
-
-
-$(".modal-button").click(function() {
-    var target = $(this).data("target");
-    $("html").addClass("is-clipped");
-    $(target).addClass("is-active");
-    });
-    
-    $(".modal-close").click(function() {
-    $("html").removeClass("is-clipped");
-    $(this).parent().removeClass("is-active");
-    });
-
 
 document.addEventListener('ContentLoaded', function(){
     updateRunCountDisplay();
