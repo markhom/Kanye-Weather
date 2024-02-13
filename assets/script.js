176843ea853bf0b62d665467d557062eb16e9eab
Amara <ajbriquelet@gmail.com>
2024-02-12 19:57:12 -0600

    removed geolocator, debugging tools added, please review

--- assets/script.js
+++ assets/script.js
@@ -3,71 +3,67 @@ document.getElementById("start").addEventListener("click", function () {
     quoteGenerator()
 });
 function quoteGenerator() {
-const url = 'https://api.kanye.rest/'; //url variable name assigned a value of: https://api.kanye.rest/
-fetch(url) //request quotes from server
-.then(response => response.json()) //json conversion 
-.then(data => {
-    document.getElementById('quoteContainer').textContent = data.quote; // id for injected quotes is "quoteContainer". 
-    //call function to automatically detect user location
-    getLocation(); 
-})
-.catch(error => console.error("error retrieving quote", error)); //error message specific to function     
+    const url = 'https://api.kanye.rest/'; //url variable name assigned a value of: https://api.kanye.rest/
+    fetch(url) //request quotes from server
+        .then(response => response.json()) //json conversion 
+        .then(data => {
+            document.getElementById('quoteContainer').textContent = data.quote; // id for injected quotes is "quoteContainer". 
+            //call function to detect user location
+            getLocation();
+        })
+
+        .catch(error => console.error("error retrieving quote", error)); //error message specific to function     
 }
+
 //function to retrieve user coordinates
 function getLocation() {
-    navigator.geolocation.getCurrentPosition( //changed to getCurrentPosition to avoid violating api rules for how many requests can be made (not more than one per second) 
-        successCall, //success condition
-        errorCall, //error only here so I can include the high accuracy object. 
+    navigator.geolocation.getCurrentPosition(
+        successCall, //success condition, runs the functions needed to populate the data table displayed on the page.
+        errorCall, //included to enable high accuracy, also provides a specific error message to allow for easier debugging. 
         {
             enableHighAccuracy: true, //options object, higher accuracy geolocation.
         }
     );
 }
-
 function successCall(position) {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
+    let coord = position.coords;
+    let vertical = coord.latitude; //latitude variable
+    let horizontal = coord.longitude; //longitude variable
 
-    //Nearest city based on coords. nominatim api + openstreetmap
-    fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`) //when the users lat and long are successfully retrieved they are used as variables here.
+    console.log(vertical, horizontal); // Check console for coordinates
+    // Summon the next function
+    fetchWeather(vertical, horizontal);
+}
+function fetchWeather(vertical, horizontal) {
+    let weatherKey = '0f155a46c630bcfc919257e207d8ff73'; // API key with specific name
+    let weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${vertical}&lon=${horizontal}&appid=${weatherKey}&units=imperial`; //plugged in lat and long
+    fetch(weatherUrl)
         .then(response => response.json())
         .then(data => {
-            let closestCity = data.address.city;
-            console.log(closestCity); //check console for city name.
-            //summon the next function
-            fetchWeather(closestCity);
-        })
-        .catch(error => console.error("error retrieving coordinates", error)); //error message specific to function
-}
-//weather api
-function fetchWeather(closestCity) {
-    let weatherKey = '0f155a46c630bcfc919257e207d8ff73'; //api key with specific name
-    let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${closestCity}&appid=${weatherKey}&units=imperial`; //url used to fetch, units=imperial is important to avoid inserting high school math into your javascript.                    
+            const weatherData = data.list[0]; 
+            console.log(weatherData); //CHECK FOR THIS TO MAKE SURE COORDS ARE SUMMONING API INFO FOR YOU. 
+            document.getElementById('temperature').textContent = `Temperature: ${weatherData.main.temp}°F`;
+            document.getElementById('realFeel').textContent = `Real Feel: ${weatherData.main.feels_like}°F`;
+            document.getElementById('weatherConditions').textContent = `Weather Conditions: ${weatherData.weather[0].description}`;
+            document.getElementById('precipitation').textContent = `Precipitation: ${weatherData.clouds.all}%`;
+            document.getElementById('visibility').textContent = `Visibility: ${weatherData.visibility}`;
+            document.getElementById('humidity').textContent = `Humidity: ${weatherData.main.humidity}%`;
 
-    fetch(weatherUrl)
-    .then(response => response.json())
-    .then (data => {
-        document.getElementById('temperature').textContent = `Temperature: ${data.main.temp}°F`; //accessing specific api data off this list: https://openweathermap.org/api/one-call-3 
-        document.getElementById('realFeel').textContent = `Real Feel: ${data.main.feels_like}°F`; 
-        document.getElementById('weatherConditions').textContent = `Weather Conditions: ${data.weather[0].description}`; 
-        document.getElementById('precipitation').textContent = `Precipitation: ${data.clouds.all}%`; 
-        document.getElementById('visibility').textContent = `Visibility: ${data.visibility}`;
-        document.getElementById('humidity').textContent = `Humidity: ${data.main.humidity}%`;              
-        addMap(data) //calling the next function, ensuring all relevant data is passed on correctly.
-    })
-    .catch(error => console.error("error fetching weather", error)); //error message specific to function
+            addMap(weatherData);
+        })
+        .catch(error => console.error("Error fetching weather", error)); // Error message specific to function
 }
-    //function below to display an icon above list of weather conditions. 
-    function addMap(weatherData) { 
-        let mapContainer = document.getElementById('theMap'); //this becomes the icon, theMap is the icon and should have a div around it in the html to work.
-        let weatherDesignater = weatherData.weather[0].icon; // finds out what the weather is and design
-        let picUrl = `https://openweathermap.org/img/wn/${weatherDesignater}.png`; //api icon url link
-        $('#theMap').attr('src', picUrl); //pushes this source to the empty html field, with #theMap being the actual image and iconPic being the li container portion (both must be present to function properly)
-       
-    }
- function errorCall(error) {
-console.log("error in getLocation function or user has rejected permissions."); 
- }
+//function below to display an icon above list of weather conditions. 
+function addMap(weatherData) {
+    let mapContainer = document.getElementById('theMap'); //this becomes the icon, theMap is the icon and should have a div around it in the html to work.
+    let weatherDesignater = weatherData.weather[0].icon; // finds out what the weather is and design
+    let picUrl = `https://openweathermap.org/img/wn/${weatherDesignater}.png`; //api icon url link
+    $('#theMap').attr('src', picUrl); //pushes this source to the empty html field, with #theMap being the actual image and iconPic being the li container portion (both must be present to function properly)
+
+}
+function errorCall(error) {
+    console.log("error in getLocation function or user has rejected permissions."); //specific error message for easier debugging. 
+}
 
 $(".modal-button").click(function () {
     var target = $(this).data("target");
@@ -88,21 +84,21 @@ $(document).ready(function () {
         $('#startContainer').hide();
         $('#kanye').show();
 
-      });
+    });
 });
 
 function getRandomKanyeQuote() {
-    
+
     quoteGenerator().then((quotes) => {
         if (quotes.length > 0) {
-            
+
             const randomIndex = Math.floor(Math.random() * quotes.length);
 
             // show randomly selected quote
             document.getElementById('quoteContainer').innerText = quotes[randomIndex];
-        } 
+        }
     }).catch((error) => {
-            // display if error
+        // display if error
         console.error("Error fetching Kanye quotes", error);
     });
 }
